baccd5fc660a915902af7ef982a3c9a5682440cf
naotohori <hori.naoto@gmail.com>
2020-03-04 11:45:02 -0600

    Convert from Py2 to Py3

--- bin/susage_users.py
+++ bin/susage_users.py
@@ -1,19 +1,5 @@
 #!/usr/bin/env python
 
-#!/bin/bash
-#h=`squeue -u hori -h | wc -l`
-#t=`squeue -u tienhung -h | wc -l`
-#k=`squeue -u mk37256 -h | wc -l`
-#m=`squeue -u maurolm -h | wc -l`
-#echo 'Hori ' $h
-#echo 'Hung ' $t
-#echo 'Marina ' $k
-#echo 'Mauro ' $m
-#let total='48*16'
-#let avail="$total-$h-$t"
-#echo 'AVAIL' $avail
-#echo 'TOTAL' $total
-
 import subprocess
 
 exe='squeue --noheader --format="%.8u %.2t %.6D %.6C"'
@@ -23,11 +9,11 @@ proc = subprocess.Popen(exe, shell=True, stdout=subprocess.PIPE)
 user_usage = {}
 
 squeue = []
-for l in iter(proc.stdout.readline, ''):
+for l in iter(proc.stdout.readline, b''):
     if l[0] == '#':
         continue
 
-    squeue.append(l.rstrip('\n'))
+    squeue.append(l.rstrip(b'\n'))
     lsp = l.split()
 
     user = lsp[0]
@@ -36,32 +22,36 @@ for l in iter(proc.stdout.readline, ''):
     core = int(lsp[3])
 
     if user in user_usage:
-        if stat == 'R':
+        if stat == b'R':
             user_usage[user][0] += node
             user_usage[user][1] += core
         else:
             user_usage[user][2] += node
             user_usage[user][3] += core
     else:
-        if stat == 'R':
+        if stat == b'R':
             user_usage[user] = [node, core,    0,    0]
         else:
             user_usage[user] = [    0,   0, node, core]
 
-print ('#             Running       Pending       Total')
-print ('#   User    Node   Core   Node   Core   Node   Core')
+print ('#        |     Running   |    Pending    |    Total')
+print ('#  User  |  Node   Core  |  Node   Core  |  Node   Core')
+print ('---------+---------------+---------------+--------------')
 total_R_node = 0
 total_R_core = 0
 total_P_node = 0
 total_P_core = 0
 total_node = 0
 total_core = 0
-for user, usage in user_usage.iteritems():
+for user, usage in user_usage.items():
     total_R_node += usage[0]
     total_R_core += usage[1]
     total_P_node += usage[2]
     total_P_core += usage[3]
-    print ("%8s %6i %6i %6i %6i %6i %6i" % (user, usage[0], usage[1], usage[2], usage[3], usage[0]+usage[2], usage[1]+usage[3]))
+    print ("%8s | %6i %6i | %6i %6i | %6i %6i" % (user.decode('utf-8'), usage[0], usage[1], usage[2], usage[3], usage[0]+usage[2], usage[1]+usage[3]))
 
-print ("%8s %6i %6i %6i %6i %6i %6i" % ('Total' , total_R_node, total_R_core,
-                                                   total_P_node, total_P_core, total_node, total_core,))
+print ('---------+---------------+---------------+--------------')
+print ("%8s | %6i %6i | %6i %6i | %6i %6i" % ('Total' , total_R_node, total_R_core,
+                                                   total_P_node, total_P_core, 
+                                                   total_R_node+total_P_node,
+                                                   total_R_core+total_P_core))
